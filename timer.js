7c32cb971d41ebdfb0024f330224842edcb08100
COZMIKDX <tacocatdx@gmail.com>
2024-04-11 16:47:36 -0500

    Create timer.js
    Moving the timer class I made over here from codepen.

---
+++ timer.js
@@ -0,0 +1,48 @@
+class DynamicTimer {
+    // interval is in milliseconds.
+    constructor(interval) {
+        this.interval = interval;
+        this.timerId = null;
+        this.expectedTime = 0;
+        this.callback = null;
+
+        this.timerTick = this.timerTick.bind(this);
+    }
+
+    timerTick() {
+        var drift = Date.now() - this.expectedTime;
+        if (drift > this.interval) {
+            // Took too long. Do anything needed to make up for being late.
+            console.log("Drift detected");
+        } else { 
+          // Normal on-time behaviour
+          this.callback();
+        }
+
+        this.expectedTime += this.interval; // The next time it should trigger ideally.
+        // setTimout again but adjust the interval to make up for drift.
+        // If the drift was too big, set the next interval to 0.
+        this.timerId = setTimeout(this.timerTick, Math.max(0, this.interval - drift));
+    }
+
+    startTimer() {
+        if (this.callback === null) { 
+          console.log("Timer callback is not set. Timer not started.");
+          return; 
+        }
+        this.expectedTime = Date.now() + this.interval;
+        this.timerId = setTimeout(this.timerTick, this.interval);
+    }
+
+    stopTimer() {
+        clearTimeout(this.timerId);
+    }
+    
+    setCallback(callback) {
+      this.callback = callback;
+    }
+  
+  setInterval(interval) {
+    this.interval = interval;
+  }
+}
